4fea15e9208e4ba03a52ff28fa237034014a3ec0
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-13 15:22:20 +0100

    Updates on Login and Sign up UI

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Login.scss'
 import movixlogo from '../../assets/movix_logo.png';
 import Input from '../../components/Input/Input';
-
+import { Link } from 'react-router-dom';
 const Login = () => {
   return (
     <div className="login_scaffold">
@@ -10,7 +10,7 @@ const Login = () => {
         <div className="login__logo"><img src={movixlogo} alt="Movix logo"/></div>
         <div className="login__caps">
           <h2>Hi, Welcome</h2>
-          <p>Please sign-up to start your experience</p>
+          <p>Please sign-in to start your experience</p>
         </div>
 
         <Input type="email"/>
@@ -20,7 +20,7 @@ const Login = () => {
         <div className="login__divider"></div>
         <div className="btn__auth">LOGIN</div>
         <div className="login__divider"></div>
-        <p className="login__register">Don't have an account? <span>Register</span></p>
+        <p className="login__register">Don't have an account? <Link to={'/signup'}>Register</Link></p>
       </div>
     </div>
   )
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Signup.scss'
 import movixlogo from '../../assets/movix_logo.png';
 import Input from '../../components/Input/Input';
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
@@ -10,7 +11,7 @@ const Signup = () => {
         <div className="signup__logo"><img src={movixlogo} alt="Movix logo"/></div>
         <div className="signup__caps">
           <h2>Hi, Welcome</h2>
-          <p>Please sign-in to start your experience</p>
+          <p>Please sign-up to start your experience</p>
         </div>
 
         <Input type="text"/>
@@ -22,7 +23,7 @@ const Signup = () => {
         <div className="signup__divider"></div>
         <div className="btn__auth">LOGIN</div>
         <div className="signup__divider"></div>
-        <p className="signup__login">Already have an account? <span>Login</span></p>
+        <p className="signup__login">Already have an account? <Link to={'/login'}>Login</Link></p>
       </div>
     </div>
   )
