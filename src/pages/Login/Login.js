dbecb9fc8ca835a3eddd7815ddd2bf8c927aa945
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-14 18:06:53 +0100

    Added react helmet for dynamicpage title rendering

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Login.scss'
 import movixlogo from '../../assets/movix_logo.png';
 import Input from '../../components/Input/Input';
 import { Link } from 'react-router-dom';
+import { Helmet } from "react-helmet";
+
 const Login = () => {
 
   const [email, setEmail] = useState("");
@@ -10,16 +12,21 @@ const Login = () => {
 
   return (
     <div className="login_scaffold">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Movix Login</title>
+        <link rel="canonical" href="https://movixfrontend.netlify.app/" />
+      </Helmet>
       <div className="login">
-        <div className="login__logo"><img src={movixlogo} alt="Movix logo"/></div>
+        <div className="login__logo"><img src={movixlogo} alt="Movix logo" /></div>
         <div className="login__caps">
           <h2>Hi, Welcome</h2>
           <p>Please sign-in to start your experience</p>
         </div>
 
-        <Input type="email" input={email} setInput={setEmail}/>
+        <Input type="email" input={email} setInput={setEmail} />
         <div className="login__divider"></div>
-        <Input type="password" input={password} setInput={setPassword}/>
+        <Input type="password" input={password} setInput={setPassword} />
         <div className="login__divider"></div>
         <div className="login__divider"></div>
         <div className="btn__auth">LOGIN</div>
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -3,22 +3,28 @@ import './Signup.scss'
 import movixlogo from '../../assets/movix_logo.png';
 import Input from '../../components/Input/Input';
 import { Link } from 'react-router-dom'
+import { Helmet } from 'react-helmet';
 
 const Signup = () => {
   return (
     <div className="signup_scaffold">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Movix Signup</title>
+        <link rel="canonical" href="https://movixfrontend.netlify.app/" />
+      </Helmet>
       <div className="signup">
-        <div className="signup__logo"><img src={movixlogo} alt="Movix logo"/></div>
+        <div className="signup__logo"><img src={movixlogo} alt="Movix logo" /></div>
         <div className="signup__caps">
           <h2>Hi, Welcome</h2>
           <p>Please sign-up to start your experience</p>
         </div>
 
-        <Input type="text"/>
+        <Input type="text" />
         <div className="signup__divider"></div>
-        <Input type="email"/>
+        <Input type="email" />
         <div className="signup__divider"></div>
-        <Input type="password"/>
+        <Input type="password" />
         <div className="signup__divider"></div>
         <div className="signup__divider"></div>
         <div className="btn__auth">LOGIN</div>
