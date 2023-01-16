e6116aae57acf8b7e22c63888bb47d7d30205f11
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-16 17:33:32 +0100

    Rendering movies from server

--- src/pages/Home/FeaturedMovie/FeaturedMovie.js
+++ src/pages/Home/FeaturedMovie/FeaturedMovie.js
@@ -16,6 +16,7 @@ import Spinner from '../../../components/Spinner/Spinner';
 const FeaturedMovie = () => {
 
     const [loading, setLoading] = useState(false)
+    const [movies, setMovies] = useState([])
     const collectionRef = useRef();
 
     const arrowLeft = () => {
@@ -29,18 +30,24 @@ const FeaturedMovie = () => {
     }
 
     useEffect(() => {
-        let collection = collectionRef.current
-
-        collection.onscroll = (e) => {
-            const rightScroll = collection.scrollLeft + collection.offsetWidth
-
-            if(rightScroll + 250 >= collection.scrollWidth){
-                setLoading(true)
-            }else{
-                setLoading(false)
-            }
-        }
+        setLoading(true)
+        fetch("https://api.themoviedb.org/3/movie/popular?api_key=11d9d6e07330833dd9c9a8c9f0a0c184&language=en-US&page=1", {
+            method: 'get'
+        })
+            .then(res => res.json())
+            .then(data => {
+                setMovies([...data.results])
+            })
+            .then(() => setLoading(false))
+            .catch(error => console.log(error))
     }, [])
+
+    console.log(movies)
+    console.log(loading)
+
+    const fetchedMovies = movies.map((movie, index)=>(
+        <MovieCard favorite_img={favorite} movie_img={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} ratings_imdb={imdb} ratings_apple={apple} title={movie.title} rating_a={"78.0"} rating_b={"94"} genre={"Animation, Drama, History"} />
+    ))
 
 
 
@@ -56,13 +63,14 @@ const FeaturedMovie = () => {
                 <div className="featured-movie__collection" ref={collectionRef}>
                     <div className="featured-movie__collection--arrow-left" onClick={arrowLeft}><img src={arrow_left} alt="arrow_left" /></div>
                     <div className="featured-movie__collection--arrow-right" onClick={arrowRight}><img src={arrow_right} alt="arrow_right" /></div>
-                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_1} ratings_imdb={imdb} ratings_apple={apple} title={"Stranger Things"} rating_a={"80.0"} rating_b={"97"} genre={"Action, Adventure, Horror"} tv_series={"TV SERIES"}/>
+                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_1} ratings_imdb={imdb} ratings_apple={apple} title={"Stranger Things"} rating_a={"80.0"} rating_b={"97"} genre={"Action, Adventure, Horror"} tv_series={"TV SERIES"} />
                     <MovieCard favorite_img={favorite} movie_img={featuredmovie_2} ratings_imdb={imdb} ratings_apple={apple} title={"Batman Begins"} rating_a={"82.0"} rating_b={"70"} genre={"Action, Adventure"} />
                     <MovieCard favorite_img={favorite} movie_img={featuredmovie_3} ratings_imdb={imdb} ratings_apple={apple} title={"Spider-Man : Into The Spider Verse"} rating_a={"84.0"} rating_b={"87"} genre={"Animation, Action, Adventure"} />
                     <MovieCard favorite_img={favorite} movie_img={featuredmovie_4} ratings_imdb={imdb} ratings_apple={apple} title={"Dunkirk"} rating_a={"78.0"} rating_b={"94"} genre={"Animation, Drama, History"} />
 
-                    
-                    {loading && <div className="featured-movie__collection--spinner"><Spinner/></div> }
+                    {fetchedMovies}
+
+                    {loading && <div className="featured-movie__collection--spinner"><Spinner /></div>}
 
                 </div>
             </div>
