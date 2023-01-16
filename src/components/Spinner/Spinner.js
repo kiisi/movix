02f953eb488ad2535527ad79676f7ae5eae3ac65
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-16 15:51:45 +0100

    Adding mixins in utils

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Spinner.scss'
+const Spinner = () => {
+  return (
+        <div className="spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+        </div>
+  )
+}
+
+export default Spinner
--- src/pages/Home/FeaturedMovie/FeaturedMovie.js
+++ src/pages/Home/FeaturedMovie/FeaturedMovie.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import './FeaturedMovie.scss'
 import imdb from '../../../assets/imdb.png'
 import apple from '../../../assets/apple.png'
@@ -10,21 +10,39 @@ import featuredmovie_4 from '../../../assets/featuredmovie_4.png'
 import arrow_right from '../../../assets/arrow_right.png'
 import arrow_left from '../../../assets/arrow_left.png'
 import MovieCard from '../../../components/MovieCard/MovieCard';
+import Spinner from '../../../components/Spinner/Spinner';
 
 
 const FeaturedMovie = () => {
 
+    const [loading, setLoading] = useState(false)
     const collectionRef = useRef();
 
-    const arrowLeft = () =>{
+    const arrowLeft = () => {
         let collection = collectionRef.current
         collection.scrollLeft -= 330
     }
 
-    const arrowRight = () =>{
+    const arrowRight = () => {
         let collection = collectionRef.current
         collection.scrollLeft += 330
     }
+
+    useEffect(() => {
+        let collection = collectionRef.current
+
+        collection.onscroll = (e) => {
+            const rightScroll = collection.scrollLeft + collection.offsetWidth
+
+            if(rightScroll + 200 >= collection.scrollWidth){
+                setLoading(true)
+            }else{
+                setLoading(false)
+            }
+        }
+    }, [])
+
+
 
     return (
         <div className="max-wrapper standard__spacing--top">
@@ -36,12 +54,16 @@ const FeaturedMovie = () => {
                 <div className="featured-movie__divider"></div>
                 <div className="featured-movie__divider"></div>
                 <div className="featured-movie__collection" ref={collectionRef}>
-                    <div className="featured-movie__collection--arrow-left" onClick={arrowLeft}><img src={arrow_left} alt="arrow_left"/></div>
-                    <div className="featured-movie__collection--arrow-right" onClick={arrowRight}><img src={arrow_right} alt="arrow_right"/></div>
+                    <div className="featured-movie__collection--arrow-left" onClick={arrowLeft}><img src={arrow_left} alt="arrow_left" /></div>
+                    <div className="featured-movie__collection--arrow-right" onClick={arrowRight}><img src={arrow_right} alt="arrow_right" /></div>
                     <MovieCard favorite_img={favorite} movie_img={featuredmovie_1} ratings_imdb={imdb} ratings_apple={apple} title={"Stranger Things"} rating_a={"80.0"} rating_b={"97"} genre={"Action, Adventure, Horror"} />
-                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_2} ratings_imdb={imdb} ratings_apple={apple} title={"Batman Begins"} rating_a={"82.0"} rating_b={"70"} genre={"Action, Adventure"}/>
-                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_3} ratings_imdb={imdb} ratings_apple={apple} title={"Spider-Man : Into The Spider Verse"} rating_a={"84.0"} rating_b={"87"} genre={"Animation, Action, Adventure"}/>
-                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_4} ratings_imdb={imdb} ratings_apple={apple} title={"Dunkirk"} rating_a={"78.0"} rating_b={"94"} genre={"Animation, Drama, History"}/>
+                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_2} ratings_imdb={imdb} ratings_apple={apple} title={"Batman Begins"} rating_a={"82.0"} rating_b={"70"} genre={"Action, Adventure"} />
+                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_3} ratings_imdb={imdb} ratings_apple={apple} title={"Spider-Man : Into The Spider Verse"} rating_a={"84.0"} rating_b={"87"} genre={"Animation, Action, Adventure"} />
+                    <MovieCard favorite_img={favorite} movie_img={featuredmovie_4} ratings_imdb={imdb} ratings_apple={apple} title={"Dunkirk"} rating_a={"78.0"} rating_b={"94"} genre={"Animation, Drama, History"} />
+
+                    
+                    {loading && <Spinner/> }
+
                 </div>
             </div>
         </div>
