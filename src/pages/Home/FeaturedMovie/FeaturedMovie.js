e38d71e7174dc4b6f3f6ba11d0df739c364ea79d
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-16 21:16:18 +0100

    Added Scrolling button effects

--- src/pages/Home/FeaturedMovie/FeaturedMovie.js
+++ src/pages/Home/FeaturedMovie/FeaturedMovie.js
@@ -19,6 +19,8 @@ const FeaturedMovie = () => {
     const [page, setPage] = useState(1)
     const [movies, setMovies] = useState([])
     const collectionRef = useRef();
+    const arrowRightRef = useRef()
+    const arrowLeftRef = useRef()
 
     const arrowLeft = () => {
         let collection = collectionRef.current
@@ -31,6 +33,26 @@ const FeaturedMovie = () => {
     }
 
     useEffect(() => {
+        let collection = collectionRef.current
+        let arrow_right = arrowRightRef.current
+        let arrow_left = arrowLeftRef.current
+
+        collection.onscroll = () =>{
+            let rightScroll = collection.scrollLeft + collection.offsetWidth
+            let leftScroll = collection.scrollLeft
+            
+            if(leftScroll > 0){
+                arrow_left.classList.remove("hide")
+            }else{
+                arrow_left.classList.add("hide")
+            }
+
+            if(rightScroll === collection.scrollWidth){
+                arrow_right.classList.add("hide")
+            }else{
+                arrow_right.classList.remove("hide")
+            }
+        }
         setLoading(true)
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=11d9d6e07330833dd9c9a8c9f0a0c184&language=en-US&page=${page}`, {
             method: 'get'
@@ -66,8 +88,8 @@ const FeaturedMovie = () => {
                 <div className="featured-movie__divider"></div>
                 <div className="featured-movie__divider"></div>
                 <div className="featured-movie__collection" ref={collectionRef}>
-                    <div className="featured-movie__collection--arrow-left" onClick={arrowLeft}><img src={arrow_left} alt="arrow_left" /></div>
-                    <div className="featured-movie__collection--arrow-right" onClick={arrowRight}><img src={arrow_right} alt="arrow_right" /></div>
+                    <div className="featured-movie__collection--arrow-left hide" ref={arrowLeftRef} onClick={arrowLeft}><img src={arrow_left} alt="arrow_left" /></div>
+                    <div className="featured-movie__collection--arrow-right" ref={arrowRightRef} onClick={arrowRight}><img src={arrow_right} alt="arrow_right" /></div>
                     <MovieCard favorite_img={favorite} movie_img={featuredmovie_1} ratings_imdb={imdb} ratings_apple={apple} title={"Stranger Things"} rating_a={"80.0"} rating_b={"97"} genre={"Action, Adventure, Horror"} tv_series={"TV SERIES"} />
                     <MovieCard favorite_img={favorite} movie_img={featuredmovie_2} ratings_imdb={imdb} ratings_apple={apple} title={"Batman Begins"} rating_a={"82.0"} rating_b={"70"} genre={"Action, Adventure"} />
                     <MovieCard favorite_img={favorite} movie_img={featuredmovie_3} ratings_imdb={imdb} ratings_apple={apple} title={"Spider-Man : Into The Spider Verse"} rating_a={"84.0"} rating_b={"87"} genre={"Animation, Action, Adventure"} />
