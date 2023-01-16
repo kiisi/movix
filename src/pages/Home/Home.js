19759dfc78ec532cb577c4a3b4ce00daf3e5cb3c
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-16 12:34:37 +0100

    Added meta data for movix home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,17 +5,23 @@ import NewArrival from './NewArrival/NewArrival'
 import Casts from './Casts/Casts'
 import Footer from '../../layout/Footer/Footer'
 import ExclusiveVideos from './ExclusiveVideos/ExclusiveVideos'
+import { Helmet } from 'react-helmet'
 
 
 const Home = () => {
   return (
     <div className="home_scaffold">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Movix</title>
+        <link rel="canonical" href="https://movixfrontend.netlify.app/" />
+      </Helmet>
       <Hero />
-      <FeaturedMovie/>
-      <NewArrival/>
-      <ExclusiveVideos/>
-      <Casts/>
-      <Footer/>
+      <FeaturedMovie />
+      <NewArrival />
+      <ExclusiveVideos />
+      <Casts />
+      <Footer />
     </div>
   )
 }
