0c06d400071c878013a62be32a99990663e87678
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-17 04:10:49 +0100

    updates on hoc logics

--- src/hoc/AuthHoc.js
+++ src/hoc/AuthHoc.js
@@ -6,55 +6,68 @@ import authSlice  from '../store/slices/authSlice'
 import './AuthHoc.scss'
 
 const AuthHoc = (props) => {
-    const [loading, setLoading] = useState(false)
     const [auth, setAuth] = useState(null)
     const authState = useSelector((state) => state.auth)
     const dispatch = useDispatch()
     const { authUpdate } = authSlice.actions
     const navigate = useNavigate()
 
-    const _tk = authState.token || localStorage.getItem("_tk")
+    const _tk = localStorage.getItem("_tk") || authState.token
+
 
     useEffect(() => {
-        setLoading(true)
+        console.log(authState.token)
+        if(!_tk){
+            return navigate('/login')
+        }
         fetch(endpoint, {
             method: 'get',
             headers: {
                 "Authorization": "Bearer " + _tk
             }
         })
-            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+                if(!res.ok){
+                    return navigate('/login')
+                }
+                return res.json()
+            })
             .then(data => {
+                console.log(data)
                 if (data.success) {
                     dispatch(authUpdate(data))
                     setAuth(true)
                 } else {
+                    navigate('/login')
                     setAuth(false)
                 }
             })
-            .then(() => setLoading(false))
             .catch(error => console.log(error))
 
-    },[_tk])
+    },[_tk, authState.token])
 
     let loader = <div className="scaffold__loading">
         <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
         </div>
     </div>
 
-    return <>
-        {
-            loading ? 
-            loader
-            :
-                auth ?
-                    props.children
-                    :
-                    auth == null ? loader : <div className="scaffold_re-login"><div><h1>Access denied</h1><p onClick={() => navigate('/login')}>Login</p></div></div>
 
-        }
-    </>
+    return <>{ auth ? props.children : loader}</>
 
 }
+
+// return <>
+//         {
+//             loading ? 
+//             loader
+//             :
+//                 auth ?
+//                     props.children
+//                     :
+//                     auth == null ? loader : <div className="scaffold_re-login"><div><h1>Access denied</h1><p onClick={() => navigate('/login')}>Login</p></div></div>
+
+//         }
+//     </>
 
 export default AuthHoc
