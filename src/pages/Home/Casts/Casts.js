dd7a63d37ad97648b1f5fc65022b8a4f9e8d36ab
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-17 01:56:23 +0100

    Updated Casts Collections

--- src/pages/Home/Casts/Casts.js
+++ src/pages/Home/Casts/Casts.js
@@ -1,66 +1,102 @@
-import React, {useRef} from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import './Casts.scss'
 import arrow_right from '../../../assets/arrow_right.png'
 import arrow_left from '../../../assets/arrow_left.png'
-import cast_1 from '../../../assets/cast_1.png'
-import cast_2 from '../../../assets/cast_2.png'
-import cast_3 from '../../../assets/cast_3.png'
-import cast_4 from '../../../assets/cast_4.png'
+import Spinner from '../../../components/Spinner/Spinner'
 
 const Casts = () => {
 
+    const [loading, setLoading] = useState(false)
+    const [page, setPage] = useState(1)
+    const [casts, setCasts] = useState([])
     const collectionRef = useRef();
+    const arrowRightRef = useRef()
+    const arrowLeftRef = useRef()
 
-    const arrowLeft = () =>{
+    const arrowLeft = () => {
         let collection = collectionRef.current
         collection.scrollLeft -= 330
     }
 
-    const arrowRight = () =>{
+    const arrowRight = () => {
         let collection = collectionRef.current
         collection.scrollLeft += 330
     }
 
-  return (
-    <div className="max-wrapper standard__spacing--top">
+    useEffect(() => {
+        let collection = collectionRef.current
+        let arrow_right = arrowRightRef.current
+        let arrow_left = arrowLeftRef.current
+
+        collection.onscroll = () => {
+            let rightScroll = collection.scrollLeft + collection.offsetWidth
+            let leftScroll = collection.scrollLeft
+
+            if (leftScroll > 0) {
+                arrow_left.classList.remove("hide")
+            } else {
+                arrow_left.classList.add("hide")
+            }
+
+            if (rightScroll === collection.scrollWidth) {
+                arrow_right.classList.add("hide")
+            } else {
+                arrow_right.classList.remove("hide")
+            }
+        }
+
+        setLoading(true)
+        fetch(`https://api.themoviedb.org/3/person/popular?api_key=11d9d6e07330833dd9c9a8c9f0a0c184&language=en-US&page=${page}`, {
+            method: 'get'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setCasts((e) => [...e, ...data.results])
+                arrow_right.classList.remove("hide")
+            })
+            .then(() => setLoading(false))
+            .catch(error => console.log(error))
+    }, [page])
+
+    const seeMoreCasts = () => {
+        if (page <= 500) {
+            setPage((e) => e + 1)
+        }
+    }
+
+
+
+    let fetchedCasts = casts.map((cast, index) => (
+        <div className="cast" key={index}>
+            <div className="cast__img">
+                <img src={`https://image.tmdb.org/t/p/w300/${cast.profile_path}`} alt={cast.name} />
+            </div>
+            <div className="cast__caption">{cast.name}</div>
+        </div>
+    ))
+
+    return (
+        <div className="max-wrapper standard__spacing--top">
             <div className="max-wrapper__content featured-casts">
                 <div className="featured-casts__header">
                     <h1>Featured Casts</h1>
-                    <span className="featured-casts__header--see-more">See more<span className="material-icons">chevron_right</span></span>
+                    <span className="featured-casts__header--see-more" onClick={seeMoreCasts}>See more<span className="material-icons">chevron_right</span></span>
                 </div>
                 <div className="featured-casts__divider"></div>
                 <div className="featured-casts__divider"></div>
                 <div className="featured-casts__collection" ref={collectionRef}>
-                    <div className="featured-casts__collection--arrow-left" onClick={arrowLeft}><img src={arrow_left} alt="arrow_left"/></div>
-                    <div className="featured-casts__collection--arrow-right" onClick={arrowRight}><img src={arrow_right} alt="arrow_right"/></div>
-                    <div className="cast">
-                        <div className="cast__img">
-                            <img src={cast_1} alt="Cast"/>
-                        </div>
-                        <div className="cast__caption">Keanu Reeves</div>
-                    </div>
-                    <div className="cast">
-                        <div className="cast__img">
-                            <img src={cast_2} alt="Cast"/>
-                        </div>
-                        <div className="cast__caption">Ryan Reynolds</div>
-                    </div>
-                    <div className="cast">
-                        <div className="cast__img">
-                            <img src={cast_3} alt="Cast"/>
-                        </div>
-                        <div className="cast__caption">Timothée Chalamet</div>
-                    </div>
-                    <div className="cast">
-                        <div className="cast__img">
-                            <img src={cast_4} alt="Cast"/>
-                        </div>
-                        <div className="cast__caption">Chloë Grace Moretz</div>
-                    </div>
+                    <div className="featured-casts__collection--arrow-left hide" ref={arrowLeftRef} onClick={arrowLeft}><img src={arrow_left} alt="arrow_left" /></div>
+                    <div className="featured-casts__collection--arrow-right" ref={arrowRightRef} onClick={arrowRight}><img src={arrow_right} alt="arrow_right" /></div>
+                    {fetchedCasts}
+                    <div className="featured-casts__collection--spinner">{loading ? <Spinner /> : <span onClick={seeMoreCasts}>more...</span>}</div>
+
+
+
                 </div>
             </div>
         </div>
-  )
+    )
 }
 
 export default Casts
