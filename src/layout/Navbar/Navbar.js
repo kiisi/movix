804167306768f5aa1e2c0da8fa7fa6e4d103ee23
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-18 00:41:44 +0100

    Updates on profile name

--- src/layout/Navbar/Navbar.js
+++ src/layout/Navbar/Navbar.js
@@ -24,8 +24,7 @@ const Navbar = () => {
     navigate('/login')
   }
 
-  let name = fullname.split(" ")[0].split("").slice(0, 5).join("")
-  console.log(name)
+  let name = fullname.split(" ")[0].split("").slice(0, 7).join("")
 
   return (
     <>
@@ -39,7 +38,7 @@ const Navbar = () => {
             <span className="material-icons">search</span>
         </div>
         <div className="nav__profile">
-            <div className="nav__profile--name">{name}</div>
+            <div className="nav__profile--name">{name}{fullname.length > 7 ? '...': ''}</div>
             <div className="nav__profile--menu" onClick={showModalBottomSheet}>
               <div className="nav__profile--menu__lines"></div>
             </div>
