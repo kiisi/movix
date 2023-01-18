5fa5619573823391faef252ceaf89ae4c811f68a
Kiisi Felix <kiisifelix06@gmail.com>
2023-01-18 02:47:36 +0100

    Handling email and password validation

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -17,7 +17,25 @@ const Signup = () => {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
+
   const submit = () =>{
+
+    if(fullname.trim() === ''){
+      return toast.error("Full name can't be blank", {
+        position: toast.POSITION.TOP_RIGHT
+      });
+    }
+
+    if (!(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))){
+      return toast.error("Invalid email", {
+        position: toast.POSITION.TOP_RIGHT
+      });
+    }
+    if(password.length < 6){
+      return toast.error("Password length: > 5", {
+        position: toast.POSITION.TOP_RIGHT
+      });
+    }
 
     setLoading(true)
     const xhr = new XMLHttpRequest()
